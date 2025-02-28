2a85cb5b511782a7d57b196c132457aa6e22c67e
Maksudul <maaksuduldish@gmail.com>
2025-02-28 14:51:59 +0600

    Data fetched and showing images by api

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello')
+function loadPhotos() {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(res => res.json())
+        .then(data => displayPhotos(data))
+}
+
+function displayPhotos(photos) {
+    const photoContainer = document.getElementById('photo-container');
+    for (const photo of photos) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h4>${photo.title}</h4>
+            <img src="${photo.url}" alt="">
+        `;
+        photoContainer.appendChild(div);
+    }
+}
+
+loadPhotos()
